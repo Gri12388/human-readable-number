312bea29eb6e3c4177f1f1d382344092772859ae
Gri12388 <gri12388@yandex.ru>
2022-02-01 10:20:38 +0300

    feat: make human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function toReadable (number) {
-  
+  const units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const dozens = ['','','twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+  function subnumber(num) {
+    if (number == 0) return 'zero';
+    if (num < 20) return units[num];
+    if (num % 10) {return dozens[Math.floor(num / 10)] + ' ' + units[num % 10];}
+    else return dozens[Math.floor(num / 10)];
+  }
+
+  if (number < 100) return subnumber(number);
+  if (number < 1000 && number % 100) return units[Math.floor(number / 100)] + ' hundred ' + subnumber(number % 100); 
+  else return units[Math.floor(number / 100)] + ' hundred';
 }
